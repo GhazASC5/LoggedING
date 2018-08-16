c53dd56b8425edeb64a4897778ca06e2a8459c09
GhazASC5 <ghazanfarshahbaz2409@gmail.com>
2018-08-16 10:08:26 -0400

    my ccommit

---
+++ userPage.js
@@ -0,0 +1,7 @@
+document.getElementById('get_file').onclick = function() {
+    document.getElementById('my_file').click();
+};
+
+$('input[type=file]').change(function (e) {
+    $('#customfileupload').html($(this).val());
+});
